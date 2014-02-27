7f7f522d2cab2887c599039849aa57faeb056dc4
EastonBaseball20 <raisnerw@gmail.com>
2014-02-27 09:28:12 -0500

    fdf

---
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i % 5 == 0);
+        }
+
+        if ((if % 3 == 0) && (i % 5 == 0))
+    }
+    Console.WriteLine("{0} FizzBuzz", i);
+            
+}
+else if (if % 3 == 0
+
+
+            Console.WriteLine("{0} Fizz", i);
+{
+            else if (i % 5 == 0)
+}
+            Console.WriteLine("{0} Buzz", i);
+{
+            else 
+}
+            Console.WriteLine(i);
+{
+            Console.ReadLine();
+}
